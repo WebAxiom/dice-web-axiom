9a7c394578a0db59dc57d5d59cad3955085f7583
Kazimieras Onaitis <kazimieras.on@gmail.com>
2018-02-21 05:40:40 +0000

    Add node

--- install.sh
+++ install.sh
@@ -20,13 +20,17 @@ if ! [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     exit 1
 fi
 
+echo "Extracting node"
+tar -zxf node.tar.gz node/
+#rm node.tar.gz
+
 echo "Extracting node modules"
 tar -zxf node_modules.tar.gz node_modules/
-rm node_modules.tar.gz
+#rm node_modules.tar.gz
 
 echo "Extracting nuxt binaries"
 tar -zxf nuxt.tar.gz .nuxt/
-rm nuxt.tar.gz
+#rm nuxt.tar.gz
 
 echo "Creating .env file"
 echo -e "HOST=$ip\nAPP_PORT=$app_port\nAPI_PORT=$api_port" > .env
