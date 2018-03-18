69a8e7d7503fc01805e8c14331c81686724cc4de
Kazimieras Onaitis <s1445757@marsala.inf.ed.ac.uk>
2018-03-18 20:29:10 +0000

    run.sh update

--- run.sh
+++ run.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
-function program_is_not_installed {
+function program_is_installed {
   # set to 1 initially
-  local return_=0
+  local return_=1
   # set to 0 if not found
-  type $1 >/dev/null 2>&1 || { local return_=1; }
+  type $1 >/dev/null 2>&1 || { local return_=0; }
   # return value
   echo "$return_"
 }
 
-ip=$(hostname --ip-address)
-app_port=${1:-3000}
-api_port=${2:-3001}
+if [ $(program_is_installed axiom) == "0" ]; then
+    echo "Axiom is not installed on the system."
+    echo "Please install it before running the script."
+    echo "More information at http://www.axiom-developer.org/"
+  else
+    ip=$(hostname --ip-address)
+    app_port=${1:-3000}
+    api_port=${2:-3001}
 
-echo -e "HOST=$ip\nAPP_PORT=$app_port\nAPI_PORT=$api_port" > .env
+    echo -e "HOST=$ip\nAPP_PORT=$app_port\nAPI_PORT=$api_port" > .env
 
-if [ $(program_is_not_installed node) == "1" ]; then
-      PATH=$PATH:$(pwd)/node/bin
+
+
+    if [ $(program_is_installed node) == "0" ]; then
+          PATH=$PATH:$(pwd)/node/bin
+    fi
+
+
+    ./node/bin/npm run api &
+    ./node/bin/npm run app
 fi
-
-./node/bin/npm start
